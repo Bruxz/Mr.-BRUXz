37cc3bb14b46c01a5d2eaf87555e45d707578fd6
Bruxz <68345658+Bruxz@users.noreply.github.com>
2021-01-05 01:13:01 -0300

    Create Script.sh

---
+++ Script.sh
@@ -0,0 +1,14 @@
+#!usr/bin/env bash
+# Criador: Mr. BRUXz﷼ <mrbruxzcontato@gmail.com> 
+# Versão: 1.0
+
+apt update 
+apt upgrade 
+clear
+
+echo "Script feito por KingMr.Bruxz!"
+
+echo -e "\e[1;32m" 
+neofetch
+echo 'Bem vindo ao seu terminal!'
+PS1='usr'
